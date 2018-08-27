e122046e4ab5feab6d46403f291bab663e154ea5
caiqianyi <270852221@qq.com>
2018-08-27 10:02:55 +0800

    增加MD5 32位大写加密方式

--- common-utils/src/main/java/com/lebaoxun/commons/utils/MD5.java
+++ common-utils/src/main/java/com/lebaoxun/commons/utils/MD5.java
@@ -64,6 +64,37 @@ public class MD5 {
 		}
 		return str;
 	}
+	/**
+	 * 32位MD5加密的大写字符串
+	 * @param originstr
+	 * @return
+	 */
+	public static String encodeByMD5ForUpperCase(String originstr) {
+		if (originstr != null) {
+			try {
+				char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		                'A', 'B', 'C', 'D', 'E', 'F' };
+				// 创建具有指定算法名称的信息摘要
+				MessageDigest md = MessageDigest.getInstance("MD5");
+				// 使用指定的字节数组对摘要进行最后的更新，然后完成摘要计算
+				byte[] results = md.digest(originstr.getBytes());
+				// 将得到的字节数组编程字符窜返回
+				// 把密文转换成十六进制的字符串形式
+	            int j = results.length;
+	            char str[] = new char[j * 2];
+	            int k = 0;
+	            for (int i = 0; i < j; i++) {
+	                byte byte0 = results[i];
+	                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+	                str[k++] = hexDigits[byte0 & 0xf];
+	            }
+	            return new String(str);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		return null;
+	}
 	
 	/**
 	 * @author sx

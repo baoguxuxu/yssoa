934cf34ea62139b183369c3add19c295287cb349
caiqianyi <mc_qianyi@126.com>
2018-12-21 14:15:01 +0800

    1

--- common-utils/src/main/java/com/lebaoxun/commons/mail/JMailSender.java
+++ common-utils/src/main/java/com/lebaoxun/commons/mail/JMailSender.java
@@ -1,15 +1,25 @@
 package com.lebaoxun.commons.mail;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.URLDataSource;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMessage.RecipientType;
+import javax.mail.internet.MimeMultipart;
+
+import com.lebaoxun.commons.exception.I18nMessageException;
  
 /**
  * 简单邮件发送器，可单发，群发。
@@ -94,20 +104,25 @@ public class JMailSender {
      * @throws AddressException
      * @throws MessagingException
      */
-    public void send(String recipient, String subject, Object content)
-        throws AddressException, MessagingException {
-	    // 创建mime类型邮件
-	    final MimeMessage message = new MimeMessage(session);
-	    // 设置发信人
-	    message.setFrom(new InternetAddress(authenticator.getUsername()));
-	    // 设置收件人
-	    message.setRecipient(RecipientType.TO, new InternetAddress(recipient));
-	    // 设置主题
-	    message.setSubject(subject);
-	    // 设置邮件内容
-	    message.setContent(content.toString(), "text/html;charset=utf-8");
-	    // 发送
-	    Transport.send(message);
+    public void send(String recipient, String subject, Object content) throws I18nMessageException {
+    	try {
+		    // 创建mime类型邮件
+		    final MimeMessage message = new MimeMessage(session);
+		    // 设置发信人
+		    message.setFrom(new InternetAddress(authenticator.getUsername()));
+		    // 设置收件人
+		    message.setRecipient(RecipientType.TO, new InternetAddress(recipient));
+		    // 设置主题
+		    message.setSubject(subject);
+		    // 设置邮件内容
+		    message.setContent(content.toString(), "text/html;charset=utf-8");
+		    // 发送
+		    Transport.send(message);
+	    } catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new I18nMessageException("－1","邮件发送失败",e);
+		}
     }
  
     /**
@@ -123,24 +138,30 @@ public class JMailSender {
      * @throws MessagingException
      */
     public void send(List<String> recipients, String subject, Object content)
-        throws AddressException, MessagingException {
+        throws I18nMessageException {
 	    // 创建mime类型邮件
 	    final MimeMessage message = new MimeMessage(session);
 	    // 设置发信人
-	    message.setFrom(new InternetAddress(authenticator.getUsername()));
-	    // 设置收件人们
-	    final int num = recipients.size();
-	    InternetAddress[] addresses = new InternetAddress[num];
-	    for (int i = 0; i < num; i++) {
-	        addresses[i] = new InternetAddress(recipients.get(i));
-	    }
-	    message.setRecipients(RecipientType.TO, addresses);
-	    // 设置主题
-	    message.setSubject(subject);
-	    // 设置邮件内容
-	    message.setContent(content.toString(), "text/html;charset=utf-8");
-	    // 发送
-	    Transport.send(message);
+	    try {
+			message.setFrom(new InternetAddress(authenticator.getUsername()));
+			// 设置收件人们
+			final int num = recipients.size();
+			InternetAddress[] addresses = new InternetAddress[num];
+			for (int i = 0; i < num; i++) {
+				addresses[i] = new InternetAddress(recipients.get(i));
+			}
+			message.setRecipients(RecipientType.TO, addresses);
+			// 设置主题
+			message.setSubject(subject);
+			// 设置邮件内容
+			message.setContent(content.toString(), "text/html;charset=utf-8");
+			// 发送
+			Transport.send(message);
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new I18nMessageException("－1","邮件发送失败",e);
+		}
     }
  
     /**
@@ -153,10 +174,48 @@ public class JMailSender {
      * @throws AddressException
      * @throws MessagingException
      */
-    public void send(String recipient, JMailTemplate mail)
-        throws AddressException, MessagingException {
+    public void send(String recipient, JMailTemplate mail){
     	send(recipient, mail.getSubject(), mail.getContent());
     }
+    
+    /**
+	 * 
+	 * @param recipient 收件邮箱地址
+	 * @param subject   主题
+	 * @param content   内容
+	 * @param file      附件
+	 * @throws AddressException
+	 * @throws MessagingException
+	 */
+	public void sendFile(String recipient, String subject, Object content, String url){
+		final MimeMessage message = new MimeMessage(this.session);
+		try {
+			message.setFrom(new InternetAddress(authenticator.getUsername()));
+			message.setRecipient(RecipientType.TO, new InternetAddress(recipient));
+			message.setSubject(subject);
+			message.setContent(content.toString(), "text/html;charset=utf-8");
+			
+			// 根据文件名获取数据源
+			// 发送附件
+			Multipart multipart = new MimeMultipart();
+			MimeBodyPart messageBodyPart = new MimeBodyPart();
+			DataSource source = null;
+			source = new URLDataSource(new URL(url));
+			String fileName = url.substring(url.lastIndexOf("/")+1);
+			messageBodyPart.setDataHandler(new DataHandler(source));
+			messageBodyPart.setFileName(fileName);
+			multipart.addBodyPart(messageBodyPart);
+			message.setContent(multipart);
+			message.saveChanges();
+			Transport.send(message);
+		} catch (MessagingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
  
     /**
      * 群发邮件
@@ -175,12 +234,7 @@ public class JMailSender {
     
 	public static void main(String[] args) {
     	JMailSender sender = new JMailSender("smtp.mxhichina.com","fengyr@i-shancan.com","1234.FYR");
-    	try {
-			sender.send("caiqy@i-shancan.com", "测试", "得分手段");
-		} catch (MessagingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		sender.send("caiqy@i-shancan.com", "测试", "得分手段");
 	}
     
 }
